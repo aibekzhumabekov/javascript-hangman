cc85e0c31e9233c1eb3900ac60792e5f15a5421c
Aibek <aibekzhv@gmail.com>
2020-06-19 23:10:48 -0500

    adding getters & setters

--- hangman.js
+++ hangman.js
@@ -26,7 +26,7 @@ class Hangman {
       this.status = "playing";
     }
   }
-  getStatusMessage() {
+  get statusMessage() {
     if (this.status === "playing") {
       return `Guesses left: ${this.remainingGuesses}`;
     } else if (this.status === "failed") {
@@ -36,7 +36,7 @@ class Hangman {
     }
   }
 
-  getPuzzle() {
+  get puzzle() {
     let puzzle = "";
 
     this.word.forEach((letter) => {
